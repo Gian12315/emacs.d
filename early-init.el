5a606a9f31f14c9e74692042d780dffb10f99836
Gian12315 <giancarlo.cytro@gmail.com>
2024-01-28 21:52:45 -0600

    A lot of changes, I don't know what I did.

--- early-init.el
+++ early-init.el
@@ -1,1 +1,9 @@
-(setq package-enable-at-startup nil)
+(defvar gian/tiling-window-manager-regexp
+  "Regular expresion to tiling window managers that I might use."
+  (regexp-opt '("stumpwm")))
+
+(defmacro gian/with-desktop-session (&rest body)
+  (declare (indent 0))
+  `(when-let ((session (getenv "DESKTOP_SESSION"))
+              ((not (string-match-p session gian/tiling-window-manager-regexp))))
+     ,@body))
